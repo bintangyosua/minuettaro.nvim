13ffba6c57fb935859f9f4a2a6e21e5b00d1bec0
Bintang <itzbintangyosua@gmail.com>
2024-05-03 11:48:49 +0700

    added format on command and format on save

---
+++ lua/plugins/nonels.lua
@@ -0,0 +1,45 @@
+return {
+  "nvimtools/none-ls.nvim",
+  config = function()
+    local null_ls = require("null-ls")
+    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+    -- setting formater yang akan dipakai
+
+    null_ls.setup({
+      sources = {
+        -- stylua
+        null_ls.builtins.formatting.stylua,
+
+        -- prettier
+        null_ls.builtins.formatting.prettier.with {
+          filetypes = {
+            "css", "html", "javascript", "typescript", "json", "yaml",
+            "graphql", "php", "java", "ruby", "rust", "toml", "prisma",
+            "sql", "go-template", "erb", "astro",
+          },
+          dynamic_command = function()
+            return "prettier"
+          end,
+        }
+      },
+
+      on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+          vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            group = augroup,
+            buffer = bufnr,
+            callback = function()
+              -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+              -- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
+              vim.lsp.buf.format({ async = false })
+              -- vim.lsp.buf.formatting_sync()
+            end,
+          })
+        end
+      end,
+    })
+
+    vim.keymap.set("n", "<leader>fm", vim.lsp.buf.format, {})
+  end
+}
